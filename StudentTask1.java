1638610da3d0f40763ce4b91567efb9b7a246006
sairam kodipyaka <149604763+KodipyakaSairam@users.noreply.github.com>
2025-03-13 11:33:13 +0530

    sample program

---
+++ StudentTask1.java
@@ -0,0 +1,18 @@
+package javaprgms;
+
+public class StudentTask1 {
+
+	void studentData(int id,String name,String course)
+	{
+		System.out.println(id+"\t"+name+"\t"+course);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StudentTask1 s=new StudentTask1();
+s.studentData(101,  "santhosh","java");
+
+	}
+
+}
